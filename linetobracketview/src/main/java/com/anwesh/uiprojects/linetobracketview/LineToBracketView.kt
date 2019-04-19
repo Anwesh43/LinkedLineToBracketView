f221156c314760b619cdc06e443509e07796b3bf
Anwesh43 <anweshthecool0@gmail.com>
2019-04-19 10:44:42 +0530

    adding LineToBrackt class

--- linetobracketview/src/main/java/com/anwesh/uiprojects/linetobracketview/LineToBracketView.kt
+++ linetobracketview/src/main/java/com/anwesh/uiprojects/linetobracketview/LineToBracketView.kt
@@ -180,4 +180,28 @@ class LineToBracketView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineToBracket(var i : Int) {
+
+        private val root : LTBNode = LTBNode(0)
+        private var curr : LTBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
