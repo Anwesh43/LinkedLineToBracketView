b06b4436f7bed661a2815b2784c8f3395a016db9
Anwesh43 <anweshthecool0@gmail.com>
2019-04-19 10:52:35 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedlinetobracketsview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinetobracketsview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedlinetobracketsview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linetobracketview.LineToBracketView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LineToBracketView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
