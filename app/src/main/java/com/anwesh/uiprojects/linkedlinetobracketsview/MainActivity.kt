76db75764060cb1e752d120221adc7e432274624
Anwesh43 <anweshthecool0@gmail.com>
2019-04-19 11:25:39 +0530

    adding transformationlistener to activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinetobracketsview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinetobracketsview/MainActivity.kt
@@ -3,13 +3,17 @@ package com.anwesh.uiprojects.linkedlinetobracketsview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linetobracketview.LineToBracketView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LineToBracketView.create(this)
+        val view : LineToBracketView = LineToBracketView.create(this)
+        view.addTransformListener({createToast("Line number $it converted to bracket")}) {
+            createToast("Bracket number $it converted to line")
+        }
         fullScreen()
     }
 }
@@ -17,4 +21,8 @@ class MainActivity : AppCompatActivity() {
 fun MainActivity.fullScreen() {
     supportActionBar?.hide()
     window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
+
+fun MainActivity.createToast(txt : String) {
+    Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
 }
