a8a5be7428b47f503c7030bd4434fe1ce8f58aca
Anwesh43 <anweshthecool0@gmail.com>
2019-04-19 10:51:33 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/linkedlinetobracketsview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinetobracketsview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinetobracketsview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linetobracketview.LineToBracketView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineToBracketView.create(this)
     }
 }
